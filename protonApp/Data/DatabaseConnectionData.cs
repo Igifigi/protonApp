6302e5090e77405778956031c22f31ac90c14032
Eastkill <witoldwoj04@gmail.com>
2022-01-02 12:09:27 +0100

    Issues Fix

--- protonApp/Data/DatabaseConnectionData.cs
+++ protonApp/Data/DatabaseConnectionData.cs
@@ -1,5 +1,10 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace protonApp.Data
 {
